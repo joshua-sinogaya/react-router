e4a1533db448ed214ea15e1fca60e6844fc6fda6
joshua-sinogaya <74108440+joshua-sinogaya@users.noreply.github.com>
2023-08-15 16:06:47 +0800

    init

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div 
+            className="ui raised very padded text container segment"
+            style={{marginTop: '80px'}}
+        >
+            <h3 className="ui header">Contact</h3>
+            <p>Duis et ex Lorem aliquip laborum consequat.</p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div 
+            className="ui raised very padded text container segment"
+            style={{marginTop: '80px'}}
+        >
+            <h3 className="ui header">Home</h3>
+            <p>Duis et ex Lorem aliquip laborum consequat.</p>
+        </div>
+    )
+}
+
+export default Home;
